4db350ad7bed3039241d3d6bba4ce403f60c9c40
Joy Phoebe <joyapisi@gmail.com>
2023-08-15 16:18:12 +0300

    Delete app

--- app/controllers/api/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::GreetingsController < ApplicationController
-    def random_greeting
-        random_message = Message.order(Arel.sql('RANDOM()')).first
-        render json: { greeting: random_message.message }
-    end
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root "messages#index"
-  resources :messages
-
-  namespace :api do
-    get 'random_greeting', to: 'greetings#random_greeting'
-  end  
-end
-
